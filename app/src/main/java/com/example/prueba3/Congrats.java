24782b998cb5c85415e5fce8d1aa85180a033499
Felipe Rodriguez Carcher <frodriguezcarcher@gmail.com>
2021-07-17 12:41:17 -0400

    register confirmation layout done

---
+++ app/src/main/java/com/example/prueba3/Congrats.java
@@ -0,0 +1,14 @@
+package com.example.prueba3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Congrats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_congrats);
+    }
+}
